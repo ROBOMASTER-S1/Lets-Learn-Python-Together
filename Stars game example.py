770e957abe8e6d2d8bf18089cf82c9c4a5fc2b02
Joseph C. Richardson <34896540+Cozmo1@users.noreply.github.com>
2020-04-03 15:19:51 -0400

    Add files via upload

---
+++ Stars game example.py
@@ -0,0 +1,26 @@
+# Type and execute/run this program example and see
+# how the while-loop only breaks when one of the two
+# 'break' statements is executed. If none of them gets
+# executed, the while-loop keeps on iterating. This
+# program example is another great example of how
+# the conditional 'if:' and 'elif:' statements work in
+# conjunction with the logical operators.
+
+while True:
+    try:
+        stars=int(input(f'How many stars would you like? ').strip())
+        if stars>1:
+            print(f'\n{stars} Stars: [',' * '*stars,f']\n\nI gave you {stars} \
+Stars!\n\nI hope you enjoy your {stars} Stars...')
+            break
+        
+        elif stars==1:
+            print(f'\n{stars} Star: [','*'*stars,f']\n\nI gave you {stars} \
+Star!\n\nI hope you enjoy your \'One\' \
+and \'Only\', single Star...')
+            break
+        
+        elif stars==0:
+            print('\nSorry Hero! Zero doesn\'t count.\n')        
+    except ValueError:
+        print('\nNumbers only please!\n')
