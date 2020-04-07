9eb96c79987ab1595bf2d89756cd0e7aa687c2b7
Joseph C. Richardson <34896540+Cozmo1@users.noreply.github.com>
2020-04-07 16:31:28 -0400

    Add files via upload

---
+++ Len_id_type examples.py
@@ -0,0 +1,23 @@
+# The 'len' function counts how many characters, including spaces there are inside of 
+# a 'print' statement. The length of these two words "Hello World!", including the
+# space in between 'Hello' and 'World!' are counted. For example: "Hello World!"
+# including one space is twelve characters long. The printout on the screen will only
+# show the number "12", not the actual words "Hello World!".
+
+print(len('Hello World!'))
+
+# Displays the id of the 'print' statement.
+
+print(id('Hello World!'))
+
+# Displays the type of the 'print' statement.
+
+print(type('Hello World!'))
+
+# Displays the id of the 'print' statement.
+
+print(id(2+3))
+
+# Displays the type of the 'print' statement.
+
+print(type(2+3))
