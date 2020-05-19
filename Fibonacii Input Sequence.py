f52b2353a1f554cc99d2bdf25a5c03c25df0f53a
Joseph C. Richardson <34896540+Cozmo1@users.noreply.github.com>
2020-05-18 23:17:48 -0400

    # input Fibonacci Number Sequence example, using a set{}

---
+++ Fibonacii Input Sequence.py
@@ -0,0 +1,34 @@
+# input Fibonacci Number Sequence example, using a set{}
+
+num1,num2=0,1
+
+fib={num1,num2}
+
+words=(
+    'is in the Fibonacci Sequence.',
+    'is not in the Fibonacci Sequence.',
+    'Please enter a correct Fibonacci Sequence Number: ',
+    'Sorry! Numbers only.',
+    'Memory Error!'
+    )
+
+try:
+    x=int(input(words[2]))
+
+    for i in range(x):
+        fib_num=num1+num2
+        fib.add(fib_num)
+        num1=num2
+        num2=fib_num
+
+    if x in fib:
+        print(x,words[0])
+        
+    elif x not in fib:
+        print(x,words[1])
+        
+except ValueError:
+    print(words[3])
+    
+except MemoryError:
+    print(words[4])
