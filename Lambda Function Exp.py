7d6a57e08063fe8dfe7369d6a49728972819e427
Joseph C. Richardson <34896540+Cozmo1@users.noreply.github.com>
2020-06-02 21:57:30 -0400

    Update Lambda Function Exp.py

--- Lambda Function Exp.py
+++ Lambda Function Exp.py
@@ -1,4 +1,4 @@
-# LAMBDA functions are 'nameless', 'anonomous' functions
+# LAMBDA functions are 'nameless', 'anonomous' functions. They are still very new to me, but at least I can cheat. lol
 
 def num(x):
     return x**3
@@ -32,3 +32,35 @@ print(d(4,7))
 
 x=lambda a,b:(a+b)**2
 print(x(3,4))
+
+x=lambda a:a+10
+print(x(5))
+
+x=lambda a,b:a*b
+print(x(5,6))
+
+x=lambda a,b,c:a+b+c
+print(x(5,6,2))
+
+def myfunc(n):
+  return lambda a:a*n
+
+mydoubler=myfunc(2)
+
+print(mydoubler(11))
+
+def myfunc(n):
+  return lambda a:a*n
+
+mytripler=myfunc(3)
+
+print(mytripler(11))
+
+def myfunc(n):
+  return lambda a:a*n
+
+mydoubler=myfunc(2)
+mytripler=myfunc(3)
+
+print(mydoubler(11)) 
+print(mytripler(11))
