f84d7bbed5491c9b35ec86e158d19e0fbf864c0c
Joseph C. Richardson <34896540+Cozmo1@users.noreply.github.com>
2020-06-07 15:40:51 -0400

    Update Abstract Python Examples.py

--- Abstract Python Examples.py
+++ Abstract Python Examples.py
@@ -449,14 +449,14 @@ print(random.random())
 
 from numpy import random
 
-x = random.randint(100)
+x=random.randint(100)
 print(x)
 
 '''----------------------------------------------------------------'''
 
 from numpy import random
 
-x = random.rand()
+x=random.rand()
 print(x)
 
 '''----------------------------------------------------------------'''
@@ -512,7 +512,7 @@ print(x)
 
 from numpy import random
 
-x=random.choice([3,5,7,9],p=[0.1,0.3,0.6,0.0],size=(3, 5))
+x=random.choice([3,5,7,9],p=[0.1,0.3,0.6,0.0],size=(3,5))
 print(x)
 
 '''----------------------------------------------------------------'''
