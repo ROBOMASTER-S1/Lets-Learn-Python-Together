9c1c4009ade2fc405184bb31dbc36408ab96cceb
Joseph C. Richardson <josephcrichardson08@gmail.com>
2021-03-02 16:10:35 -0500

    Add files via upload

---
+++ Ascii the Evil Fox.py
@@ -0,0 +1,12 @@
+# Look what I did by accident. I then created this
+# spooky Python program example.
+
+fox=ord('f')+ord('o')+ord('x')
+
+print(fox*2)
+
+# Or this example if you like.
+
+fox=(ord('f')+ord('o')+ord('x'))*2
+
+print(fox)
