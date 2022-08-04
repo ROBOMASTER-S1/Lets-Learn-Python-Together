b780be1088f4aea87f60a25f42e60c0097aa1506
Joseph C. Richardson <josephcrichardson08@gmail.com>
2022-08-04 06:30:23 -0400

    Update Binary Beat.py

--- Binary Beat.py
+++ Binary Beat.py
@@ -11,15 +11,16 @@ yellow='\x1b[33m'
 purple='\x1b[35m'
 white='\x1b[37m'
 
-text=f'binary base 2, hexadecimal \
+text=f'binary base 2, Octal base 8, hexadecimal \
 base 16, decimal base 10 systems:\n'.title()
 
 a=0
 while True:
-    print(purple+'\n'+' '*6+text+blue,'\n',' '*20,len(f'{a:b}'),green+\
+    print(purple+'\n'+' '*2+text+blue,'\n',' '*20,len(f'{a:b}'),green+\
           'binary digits: '.title()+yellow+f'{a:b} '+red+'=\n',' '*22+green+\
+          ' octal digits: '.title()+yellow+f'{a:o} '+red+'=\n',' '*22+green+\
           ' hexadecimal: '.title()+yellow+f'{a:X} '+red+'= '+green,'\n',' '*22,
           f'decimal: '.title()+red+'= '+yellow+f'{a:d}')
-    delay(1)
+    delay(.5)
     os.system('cls')
     a+=1
