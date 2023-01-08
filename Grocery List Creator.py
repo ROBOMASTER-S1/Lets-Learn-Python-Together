4cedbc4d416f994a1998b745caa1229a09bd8a26
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2023-01-08 03:10:15 -0500

    Add files via upload

--- Grocery List Creator.py
+++ Grocery List Creator.py
@@ -1,80 +1,109 @@
 '''
+Grocery List Creator Python program
+
+Created by Joseph C. Richardson
+
 This is a full functional grocery list Python program example to add to my Python book.
 Feel free to copy this Python program example if you like. See what you can do with it.
-As time rolls on, I will make this an actual, working program, which the user can place
-the price of a grocery list item, while it keeps track of what you will be spending at the
-store. The user will also be able to clear the list items or clear just one single list item,
-while the price part of the program will work as addition and subtraction of list pricing,
-via items added or removed from the shopping list. You are all welcome to copy this
-program and make it do and be anything you like. Maybe try and create the list, such as
-what I want to make it do down the road. Please enjoy.
+Please enjoy.
 '''
 import os
 
-title='title Grocery List Creator'
+title='title grocery list creator'
 clear_screen='cls'
 single_line_break='\n'
 double_line_break='\n\n'
 indent=' '*2
 dot_space='.'*3
 create_list='vgl'
+enter=0
+decimal_point=100
 button1='y'
 button2='n'
-enter=''
-
-os.system(title)
 
 sentence=[
-f'{single_line_break}{indent}Grocery List Creator', # index 0
+f'{single_line_break}{indent}grocery list creator', # index 0
 
 f'''{double_line_break}{indent}Press "Enter" after each grocery list item typed.
 {indent}Type "vgl", then press "Enter" to view your grocery list output.''', # index 1
 
 f'{double_line_break}{indent}Please type your grocery list items: ', # index 2
 
-f'{single_line_break}{indent}You have', # indext 3
+f'{double_line_break}{indent}Please type your grocery list item price: $', # index 3
 
-'items in your grocery list.', # index 4
+f'{double_line_break}{indent}You have', # indext 4
 
-f'{double_line_break}{indent}Here is your grocery list output:', # index 5
+'items in your grocery list.', # index 5
+
+f'{double_line_break}{indent}Here is your grocery list output:', # index 6
+
+f'{single_line_break}{indent}Your grocery list total:', # index 7
 
 f'''{single_line_break}{indent}Do you wish to add more items to your grocery list?
-{indent}Press "y" or "n" to confirm: ''' # index 6
+{single_line_break}{indent}Press "y" or "n" to confirm: ''', # index 8
+
+f'{single_line_break}{indent}thank you for choosing grocery list creator... \
+press enter to exit.', # index 9
 ]
 
-user_data=set()
-user_data.add(enter)
+user_input_item_data=[ ]
+user_input_price_data=[ ]
+
+user_input_item_data.append(enter)
+user_input_price_data.append(enter)
+
+os.system(title.title())
 
 def items_list():
+
     while True:
         os.system(clear_screen)
-        grocery_list=input(sentence[0]\
+        grocery_list=input(sentence[0].title()
         +sentence[1]+sentence[2]).strip()
-        user_data.add(grocery_list)
+        user_input_item_data.append(grocery_list)
+
         if grocery_list==create_list:
-            user_data.remove(create_list)
-            convert=list(user_data)
-            convert.remove(enter)
-            convert.sort()
+            user_input_item_data.remove(create_list)
+            item=user_input_item_data
+            item.remove(enter)
             break
 
+        try:
+            os.system(clear_screen)
+            item_price=float(input(sentence[0].title()
+            +sentence[1]+sentence[3]).strip())
+            user_input_price_data.append(item_price)
+        except ValueError:
+            user_input_price_data.append(0)
+
     while True:
         os.system(clear_screen)
-        print(sentence[0]+sentence[5])
+        print(sentence[0].title()+sentence[6])
 
-        x=1
-        for i in convert:
-            print(f'{single_line_break}{indent}{x}) {dot_space}',i.title())
-            x+=1
-            
-        print(sentence[3],len(convert),sentence[4])
-        grocery_list=input(sentence[6]).lower().strip()
+        try:            
+            x=1
+            for i in item:
+                print(f'{single_line_break}{indent}{x}) {dot_space} {i} \
+${user_input_price_data[x]/decimal_point}'.title())
+                x+=1
+        except IndexError:
+            item.remove(enter)
+
+        total_sum=user_input_price_data
         
+        print(f'{sentence[7]} ${sum(total_sum)/decimal_point}',
+              sentence[4],len(item),sentence[5])
+
+        grocery_list=input(sentence[8]).lower().strip()
+
+        user_input_item_data.append(enter)
+
         if grocery_list==button1:
-            items_list()
+            items_list()            
             break
         elif grocery_list==button2:
             break
-    
+
 items_list()
-input('end')
+os.system(clear_screen)
+input(sentence[9].title())
