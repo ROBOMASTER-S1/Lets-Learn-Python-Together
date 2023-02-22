3c562f5f162da60fc827e55287f259f89a634e77
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2023-02-21 20:58:51 -0500

    Update Ascii the Evil Fox.py

--- Ascii the Evil Fox.py
+++ Ascii the Evil Fox.py
@@ -1,12 +1,27 @@
-# Look what I did by accident. I then created this
-# spooky Python program example.
+'''Minute Python Program Examples'''
+
+# Try this Bone Chilling Ascii Code Python Program Example if You Dare!
 
 fox=ord('f')+ord('o')+ord('x')
 
-print(fox*2)
+print('The Devil\'s Number:',fox*2)
 
 # Or this example if you like.
 
 fox=(ord('f')+ord('o')+ord('x'))*2
 
-print(fox)
+print('The Devil\'s Number:',fox)
+
+# Here are some string concatenation examples:
+
+print('Stay away from the Evil',fox,'Python Program Example!') # non formatted string example
+
+print('Stay away from the Evil '+str(fox)+' Python Program Example!') # non formatted string example
+
+print('Stay away from the Evil '+str(fox),'Python Program Example!') # non formatted string example
+
+# Here are the old and the new Python formatted string examples
+
+print('Stay away from the Evil {} Python Program Example!'.format(fox)) # old formatted string example
+
+print(f'Stay away from the Evil {fox} Python Program Example!') # new formatted string example
