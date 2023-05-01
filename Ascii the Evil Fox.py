e70eb791342527ab95dba39de0dce60270ad951b
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2023-05-01 18:43:08 -0400

    Update Ascii the Evil Fox.py

--- Ascii the Evil Fox.py
+++ Ascii the Evil Fox.py
@@ -2,6 +2,24 @@
 
 # Try this Bone Chilling Ascii Code Python Program Example if You Dare!
 
+'''
+This is my uncut version of this scary, spooky Python program example, which this video does not
+show in it. Had I created and launched it on Halloween, I would have done such an uncut version
+like this Python program example shows. However, I couldn't let December, Friday the 13'th go by
+and by accident, I created one of my Minute Python videos #13 was the unlucky, but lucky enough
+number, that I didn't realize what I had done, until after I launched it up on YouTube that day.
+
+I was simply bored right out of my skull, when I created the Devil's Python program example. Mind
+you, it did scare me to want to even try to think of things like this up. But sometimes, my mind takes
+me to such wonderous and scary things in me, that I cannot seem to ignore them at all. Yet, having 
+Asperger's, I'm often more fearful of people than I am with a Manmade God or a Manmade Devil, 
+which I do not believe in such things at all. Mankind is simply both of these things inside us all.
+The only thing is,  which voice within you do you want to listen to. Good, or Evil? We are BOTH.
+One cannot live without the other, but the betterment of the two will always prevail as long as we
+want to survive as a species as a whole.
+
+The Devil once liveD. Don't livE Evil.
+'''
 # Created by Joseph C. Richardson, GitHub.com
 
 fox=ord('f')+ord('o')+ord('x')
