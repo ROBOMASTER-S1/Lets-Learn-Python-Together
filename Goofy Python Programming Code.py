33f427573b3ba808f649a894b47699ab20965ce8
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-01-11 03:24:53 -0500

    Update Goofy Python Programming Code.py

--- Goofy Python Programming Code.py
+++ Goofy Python Programming Code.py
@@ -9,12 +9,9 @@ loop=range
 sentence='type a list of five words: '.capitalize(),\
           'you have all'.capitalize(),'items in your list of words.'
 
-for i in loop(1,6):
-    message=text[1](sentence[0]).title().strip()
-    x.append(message)
-    x.sort()
+for i in loop(1,6):message=text[1](sentence[0]).title().strip();x.append(message)
 
-for j in loop(i):
-    text[0](x[j],end=' ')
+text[0]()
+for j in loop(i):x.sort(),text[0](x[j],end=' ')
 
 text[0](dline_brk+sentence[1],i,sentence[2])
