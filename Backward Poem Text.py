30d2569d9662b38e6ed2cb36f8488abd3652686c
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-01-21 16:49:02 -0500

    Add files via upload

---
+++ Backward Poem Text.py
@@ -0,0 +1,21 @@
+# Backward Poem Text
+
+poem = ('''‘Knowledge’
+is a free invention of the heart and of the mind itself!
+The only textbooks needed, are the heart and the mind.
+The only exam to be written is the key to ponder into wonder.
+For the heart and the mind hold the key to the greatest diploma of all,
+the dream’s creation of our imagination.
+For the heart and the mind are thus, the greatest teachers of us…
+Believe in yourself! For you are their greatest student.'''[::-1],
+
+'''.tneduts tsetaerg rieht era uoy roF !flesruoy ni eveileB
+…su fo srehcaet tsetaerg eht ,suht era dnim eht dna traeh eht roF
+.noitanigami ruo fo noitaerc s’maerd eht
+,lla fo amolpid tsetaerg eht ot yek eht dloh dnim eht dna traeh eht roF
+.rednow otni rednop ot yek eht si nettirw eb ot maxe ylno ehT
+.dnim eht dna traeh eht era ,dedeen skoobtxet ylno ehT
+!flesti dnim eht fo dna traeh eht fo noitnevni eerf a si
+’egdelwonK‘'''[::-1])
+
+print(poem[0])
