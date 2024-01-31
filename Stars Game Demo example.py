9b03dcb1540a0ee942f425bedd6203ec22ece3a6
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-01-31 15:24:50 -0500

    Update and rename Stars game example.py to Stars Game Demo example.py

--- Stars Game Demo example.py
+++ Stars Game Demo example.py
@@ -1,3 +1,9 @@
+# STARS Game Demo Python program example.
+
+# Created by Joseph C. Richardson, GitHub.com
+
+# HIGHLIGHT AND COPY CODE, THEN PASTE INTO YOUR PREFERABLE PYTHON APP/IDLE
+
 # Type and execute/run this program example and see
 # how the while-loop only breaks when one of the two
 # 'break' statements is executed. If none of them gets
@@ -13,14 +19,14 @@ while True:
             print(f'\n{stars} Stars: [',' * '*stars,f']\n\nI gave you {stars} \
 Stars!\n\nI hope you enjoy your {stars} Stars...')
             break
-        
+
         elif stars==1:
             print(f'\n{stars} Star: [','*'*stars,f']\n\nI gave you {stars} \
 Star!\n\nI hope you enjoy your \'One\' \
 and \'Only\', single Star...')
             break
-        
+
         elif stars==0:
-            print('\nSorry Hero! Zero doesn\'t count.\n')        
+            print('\nSorry Hero! Zero doesn\'t count.\n')
     except ValueError:
         print('\nNumbers only please!\n')
