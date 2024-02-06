6cbce93ed68e82fdad9fea6ca1c77777ba18705a
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-02-06 13:02:36 -0500

    Update Ascii the Evil Fox.py

--- Ascii the Evil Fox.py
+++ Ascii the Evil Fox.py
@@ -1,7 +1,4 @@
-'''Minute Python Program Examples'''
-
 # Try this Bone Chilling Ascii Code Python Program Example if You Dare!
-
 '''
 This is my uncut version of this scary, spooky Python program example, which this video does not
 show in it. Had I created and launched it on Halloween, I would have done such an uncut version
@@ -10,9 +7,9 @@ and by accident, I created one of my Minute Python videos #13 was the unlucky, b
 number, that I didn't realize what I had done, until after I launched it up on YouTube that day.
 
 I was simply bored right out of my skull, when I created the Devil's Python program example. Mind
-you, it did scare me to want to even try to think of things like this up. But sometimes, my mind takes
-me to such wonderous and scary things in me, that I cannot seem to ignore them at all. Yet, having 
-Asperger's, I'm often more fearful of people than I am with a Manmade God or a Manmade Devil, 
+you, it did scare me to want to even try to think of things like this up. But sometimes, my mind
+takes me to such wonderous and scary things in me, that I cannot seem to ignore them at all. Yet,
+having Asperger's, I'm often more fearful of people than I am with a Manmade God or a Manmade Devil, 
 which I do not believe in such things at all. Mankind is simply both of these things inside us all.
 The only thing is,  which voice within you do you want to listen to. Good, or Evil? We are BOTH.
 One cannot live without the other, but the betterment of the two will always prevail as long as we
@@ -24,32 +21,24 @@ The Devil once liveD. Don't livE Evil.
 
 fox=ord('f')+ord('o')+ord('x')
 
-char_num=102,111,120
-
-print('The Devil\'s Lucky Number:' ,(102+111+120)*2)
-
-print('The Devil\'s Lucky Number: '+str((102+111+120)*2))
-
-print('The Devil\'s Lucky Number:',int(char_num[0]+char_num[1]+char_num[2])*2)
-
-print('The Devil\'s Lucky Number:',fox*2)
+print(':rebmuN ykcuL s\'liveD ehT'[::-1],fox*2)
 
 # Or this example if you like.
 
 fox=(ord('f')+ord('o')+ord('x'))*2
 
-print('The Devil\'s Lucky Number:',fox)
+print(':rebmuN ykcuL s\'liveD ehT'[::-1],fox)
 
 # Here are some string concatenation examples:
 
-print('Stay away from the Evil',fox,'Python Program Example!') # non formatted string example
+print('livE eht iicsA morf yawa yatS'[::-1],fox,'!elpmaxE margorP nohtyP'[::-1]) # non formatted string example
 
-print('Stay away from the Evil '+str(fox)+' Python Program Example!') # non formatted string example
+print(' livE eht iicsA morf yawa yatS'[::-1]+str(fox)+'!elpmaxE margorP nohtyP '[::-1]) # non formatted string example
 
-print('Stay away from the Evil '+str(fox),'Python Program Example!') # non formatted string example
+print(' livE eht iicsA morf yawa yatS'[::-1]+str(fox),'!elpmaxE margorP nohtyP'[::-1]) # non formatted string example
 
 # Here are the old and the new Python formatted string examples
 
-print('Stay away from the Evil {} Python Program Example!'.format(fox)) # old formatted string example
+print('!elpmaxE margorP nohtyP {} livE eht iicsA morf yawa yatS'.format(fox)[::-1]) # old formatted string example
 
-print(f'Stay away from the Evil {fox} Python Program Example!') # new formatted f' string example
+print(f'!elpmaxE margorP nohtyP {fox} livE eht iicsA morf yawa yatS'[::-1]) # new formatted string example
