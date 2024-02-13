300f55f59aa81bc1f010df720badcc32e3b8f638
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-02-13 15:18:40 -0500

    Update Fibonary Bits In Action.py

--- Fibonary Bits In Action.py
+++ Fibonary Bits In Action.py
@@ -1,15 +1,17 @@
+# Fibonary Bits In Action! Python program example.
+
+# Created by Joseph C. Richardson, GitHub.com
+
+# HIGHLIGHT AND COPY CODE, THEN PASTE INTO YOUR PREFERABLE PYTHON APP/IDLE
+
 # This is for advanced Python programmers, who want something a little bit saltier.
 # Create this Fibonary Bits In Action! Python program using a single print() function.
 # Use the backslash '\' to create line breaks within the print() function.
 
-# Type and execute/run this Python program example below and see what happens.
-
 # Note: after you save your file, you must double click this file to view it's cool coloured
 # text and layout.
 
-# Created by Joseph C. Richardson
-
-import os;os.system('title fibonary bits in action! 1'.title())
+import os;os.system('title fibonary bits in action! 3'.title())
 from time import sleep as delay
 
 red='\x1b[31m'
@@ -19,8 +21,9 @@ yellow='\x1b[33m'
 purple='\x1b[35m'
 white='\x1b[37m'
 
-title_text=f'fibonary bits in action! 1'.title(),'fibonacci natural number sequence'.title()
-text=('binary digits: ','octal digits: ','hexadecimal digits: ','decimal digits:','fibonacci digits: '.title())
+title_text=f'fibonary bits in action! 3'.title(),'fibonacci natural number sequence'.title()
+text=(' binary digits: ',' octal digits: ',' hexadecimal digits: ',' decimal digits:',
+      ' fibonacci digits: '.title())
 
 lb='\n';lbb='\n\n';elb=' =\n';eq=' = ';sp=' '
 
@@ -38,11 +41,14 @@ while True:
     b=f'{num3:b}';o=f'{num3:o}'
     x=f'{num3:X}';d=f'{num3:d}'
 
-# Non formatted print() function example:
+# old string formatted print() function example:
 
-    print(white+lb+sp*16+title_text[0]+lb+red+lb+sp*3,len(b),green+text[0].title()+
-          yellow+b+blue+elb+sp*3+green+red,len(o),green+text[1].title()+yellow+
-          o+blue+elb+sp*3+green+red,len(x),green+text[2].title()+yellow+x+
-          blue+eq+green+lb+sp*3+red,len(d),green+text[3].title()+blue+eq+yellow+
-          d+lbb+white+sp*11+title_text[1]+lbb+green+sp*4+text[4]+yellow+f'{num3:,}')
+    print('{}{}{}{}{}{}{}{}{}{}{}'.format(
+        white+lb+sp*16+title_text[0]+lb+red+lb+sp*4,
+        len(b),green+text[0].title()+yellow+b+blue+elb+sp*4+
+        green+red,len(o),green,text[1].title()+yellow+o+blue+
+        elb+sp*4+green+red,len(x),green+text[2].title()+yellow+
+        x,blue+eq+green+lb+sp*4+red,len(d),green+text[3].title()+
+        blue+eq+yellow+d+lbb+white+sp*11+title_text[1]+lbb+green+
+        sp*3+text[4]+yellow+f'{num3:,d}'))    
     delay(pause)
